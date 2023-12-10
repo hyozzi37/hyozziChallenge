3ed9e50035ad0b25919cb97122a173f555839b42
hyozzi37 <149348381+hyozzi37@users.noreply.github.com>
2023-12-11 01:07:55 +0900

    Update greetings.js
    오타 수정

--- js/greetings.js
+++ js/greetings.js
@@ -1,4 +1,4 @@
-const loginFrom = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
@@ -7,22 +7,23 @@ const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginFrom.classList.add(HIDDEN_CLASSNAME);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
 
 function paintGreetings(username) {
-  greeting.innerText = `Hello ${savedUsername}`;
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginFrom.classList.remove(HIDDEN_CLASSNAME);
-  loginFrom.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   paintGreetings(savedUsername);
 }
+
